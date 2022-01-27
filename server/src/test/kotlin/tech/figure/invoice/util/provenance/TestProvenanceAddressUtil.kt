d499253f9c5ebc9fcaca2205d95e8328a8b50c47
hyperschwartz <jschwartz@figure.com>
2022-01-27 13:19:46 -0800

    Verify that the amount values are working as intended

--- server/src/test/kotlin/tech/figure/invoice/util/provenance/TestProvenanceAddressUtil.kt
+++ server/src/test/kotlin/tech/figure/invoice/util/provenance/TestProvenanceAddressUtil.kt
@@ -4,14 +4,18 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class TestProvenanceAddressUtil {
+    private companion object {
+        private const val SAMPLE_DENOM = "invoice-816c4384-7f8c-11ec-949d-afb68037fcad"
+        private const val SOURCE_ADDRESS_BODY = "1hyt8cwsqpgeajjxy92098tn27uyuqp25"
+    }
+
     @Test
-    fun testExpectedOutputBasedOnChainEnvironment() {
+    fun testExpectedOutputBasedOnSourceAddress() {
         val sourceAddress = "tp172yscg9eu72hknhue4sae5z3yyddxlfsfntcys"
-        val sampleDenom = "invoice-816c4384-7f8c-11ec-949d-afb68037fcad"
         assertEquals(
             expected = "tp1hyt8cwsqpgeajjxy92098tn27uyuqp25d30rdv",
             actual = ProvenanceAddressUtil.generateMarkerAddressForDenomFromSource(
-                denom = sampleDenom,
+                denom = SAMPLE_DENOM,
                 accountAddress = sourceAddress,
             ),
             message = "Expected the proper output when a tp address is provided",
@@ -24,6 +28,26 @@ class TestProvenanceAddressUtil {
             expected = "tp1pr93cqdh4kfnmrknhwa87a5qrwxw9k3dhkszp0",
             actual = ProvenanceAddressUtil.generateMarkerAddressForDenom("nhash", "tp"),
             message = "Expected to see the proper nhash address",
+        )
+    }
+
+    @Test
+    fun testExpectedValuesBasedOnHRPTestNet() {
+        val expectedTestNetCheckSum = "d30rdv"
+        assertEquals(
+            expected = "tp$SOURCE_ADDRESS_BODY$expectedTestNetCheckSum",
+            actual = ProvenanceAddressUtil.generateMarkerAddressForDenom(SAMPLE_DENOM, "tp"),
+            message = "Expected the testnet output to be derived with the appropriate algorithm",
+        )
+    }
+
+    @Test
+    fun testExpectedValuesBasedOnHRPMainNet() {
+        val expectedMainNetCheckSum = "76200x"
+        assertEquals(
+            expected = "pb$SOURCE_ADDRESS_BODY$expectedMainNetCheckSum",
+            actual = ProvenanceAddressUtil.generateMarkerAddressForDenom(SAMPLE_DENOM, "pb"),
+            message = "Expected the mainnet output to be derived with the appropriate algorithm",
         )
     }
 }
