a03203de75df6afeba52813ab5e3844ff010fd76
hyperschwartz <jschwartz@figure.com>
2022-02-01 15:25:28 -0800

    Add localhost cors for port 3000 when developing locally to allow the frontend local communication access

--- server/src/main/kotlin/io/provenance/invoice/config/web/WebConfig.kt
+++ server/src/main/kotlin/io/provenance/invoice/config/web/WebConfig.kt
@@ -1,17 +1,27 @@
 package io.provenance.invoice.config.web
 
+import io.provenance.invoice.config.app.ServiceProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig {
     @Bean
-    fun webRequestInterceptorConfig(): WebMvcConfigurer = object : WebMvcConfigurer {
+    fun webRequestInterceptorConfig(props: ServiceProperties): WebMvcConfigurer = object : WebMvcConfigurer {
         override fun addInterceptors(registry: InterceptorRegistry) {
             super.addInterceptors(registry)
             registry.addInterceptor(WebRequestLoggingInterceptor())
+        }
+
+        override fun addCorsMappings(registry: CorsRegistry) {
+            // Allow CORS from localhost:3000 on all routes during development.  Allows the invoice frontend to properly
+            // communicate with the backend in a local environment
+            if (props.environment == "development") {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+            }
         }
     }
 }
